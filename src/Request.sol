0772bc2b3dda7eed0f2f0d643f0d85fd7f11a39f
nutcas3 <mauricee423@gmail.com>
2024-05-07 00:46:18 +0300

    new file:   src/Request.sol

---
+++ src/Request.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract DataConsumerV3 {
+    AggregatorV3Interface internal dataFeed;
+
+    constructor() {
+        dataFeed = AggregatorV3Interface(
+            // 0xd0D5e3DB44DE05E9F294BB0a3bEEaF030DE24Ada // polygon
+            0x0715A7794a1dc8e42615F059dD6e406A6594651A // eth
+        );
+    }
+
+    function getLatestData() public view returns (int) {
+        // prettier-ignore
+        (
+            /* uint80 roundID */,
+            int answer,
+            /*uint startedAt*/,
+            /*uint timeStamp*/,
+            /*uint80 answeredInRound*/
+        ) = dataFeed.latestRoundData();
+        return answer;
+    }
+}
