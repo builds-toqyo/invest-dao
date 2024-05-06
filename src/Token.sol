2943517627aa43886b00b51fd6f30a77f8f5e489
nutcas3 <mauricee423@gmail.com>
2024-05-07 00:29:42 +0300

    feat added Token from erc20 code

---
+++ src/Token.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.8.2 <0.9.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract Token is ERC20 {
+    constructor(
+        uint256 initalCoins,
+        address _initialUser
+    ) ERC20("SmeCoin", "SME") {
+        _mint(_initialUser, initalCoins);
+    }
+
+    function burn(address account, uint256 amount) public {
+        _burn(account, amount);
+    }
+}
